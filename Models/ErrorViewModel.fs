f835c281f2bd824dc99723f24a223ace23978362
gosai09 <gosai09@gmail.com>
2022-08-16 13:14:25 -0500

    init first

---
+++ Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace Practical_1.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
